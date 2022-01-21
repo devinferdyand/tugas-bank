371121a22d4cc5e5578c3c8db52cc8a2bc1b9532
devinferdyand <devinferdynand00@gmail.com>
2022-01-21 08:03:18 +0700

    tugas bank

---
+++ index.js
@@ -0,0 +1,23 @@
+function submit(){
+    var setor = parseInt(document.getElementById("setor").value);
+
+// cara menjoinkan
+var arr = [];
+
+var ngendap = 50; 
+arr.push (setor);
+
+arr.join ('')
+
+var jumlah =Number(setor) +Number(ngendap);
+document.write(jumlah)
+
+if (setor < 50){
+alert ("gagal")
+
+var kem = Number(jumlah) + Number(tarik)
+document.write(kem)
+
+}
+document.write('<a href="tarik-tunai.html">haha</a>')
+}
